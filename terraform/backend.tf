61d9ebc1524443fa71cc9bc7467e363a86610522
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2025-07-18 18:45:22 -0400

    Formatted backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "iam-policy-generator-tf-state"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "iam-policy-generator-tf-state"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
